11363731b97811b8b78944862c73e81ad715797c
ofektabari89 <ofektabari89@gmail.com>
2025-07-19 13:36:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
